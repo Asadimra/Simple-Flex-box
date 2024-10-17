13e843eea1be7b726312dbb8274519e5ca61f995
Asadimra <asadimran031@gmail.com>
2024-10-17 17:14:09 +0500

    upload a task

---
+++ app/Main.tsx
@@ -0,0 +1,30 @@
+export default function Main(){
+    return(
+        <div>
+        <main>
+            <div className="p">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus labore cupiditate sit, corrupti eos consequatur, inventore ullam temporibus deleniti a, nobis officia est soluta praesentium vel blanditiis ipsam laborum itaque?</p>
+            </div>
+             <div className="flex justify-center gap-5 bg-gray-900 w-3/4 mx-40 py-8">
+            <div className=" w-80 px-2">
+                <img src="https://www.governorsindh.com/_next/static/media/imageWebsite.5c6ae62f.jpg" alt="" /></div>  
+                <div className="w-80 ">
+                    <img src="https://www.governorsindh.com/_next/static/media/imageWebsite2.a102c7b5.jpg" alt="" />
+                </div>
+                <div className="w-80 ">
+                    <img src="https://www.governorsindh.com/_next/static/media/imageWebsite3.b845fe78.jpg" alt="" />
+                </div>
+                </div>
+                <div className="flex justify-center bg-gray-400">
+                    <div className=" m-9 w-2/5">
+                    <img src="https://www.governorsindh.com/_next/static/media/slideShow8.4b79537a.jpg" alt="" /></div>
+                    <div className=" m-9 w-3/5 ">
+                    <img src="https://www.governorsindh.com/_next/static/media/slideShow5.b502aa01.jpg" alt="" />
+                    </div>
+                </div>
+                 
+        </main>
+        </div>
+
+    )
+}
